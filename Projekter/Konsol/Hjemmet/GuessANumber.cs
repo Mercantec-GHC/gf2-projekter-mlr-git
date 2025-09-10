e7a7710f66e9f91da58d8a86c191f07a598c9bce
mlr-git <m.l.r@tutamail.com>
2025-09-10 14:53:09 +0200

    Update - Dag 4
    Test - Github push

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -5,6 +5,11 @@ namespace Hjemmet
         public void Start()
         {
             Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+            
+            Random newRandom = new Random();
+            int randomNumber = newRandom.Next(1, 100);
+            Console.WriteLine($"tallet er: {randomNumber}");
+
             Console.ReadKey();
         }
     }
