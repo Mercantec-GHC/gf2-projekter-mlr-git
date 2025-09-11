fb6d5017fc2076269242d4fee67f75d7398e5ee5
mlr-git <m.l.r@tutamail.com>
2025-09-11 10:08:24 +0200

    Projekt opgave update
    Projekt kode-del opdateret: Gæt et tal er implementeret.

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -4,12 +4,42 @@ namespace Hjemmet
     {
         public void Start()
         {
-            Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+            //Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
             
             Random newRandom = new Random();
             int randomNumber = newRandom.Next(1, 100);
-            Console.WriteLine($"tallet er: {randomNumber}");
+            
+            bool isValid = false;
+            Console.WriteLine($"\nGæt et tal - indtast et heltal:");
+            string brugerInput = Console.ReadLine();
+            int number = 0;
 
+            while (!isValid)
+            {
+                if(int.TryParse(brugerInput, out number))
+                {
+                    if(number < randomNumber)
+                    {
+                        Console.WriteLine($"\nDit gæt er lavere end tallet - gæt igen");
+                        brugerInput = Console.ReadLine();
+                    }
+                    else if(number > randomNumber)
+                    {
+                        Console.WriteLine($"\nDit gæt er højere end tallet - gæt igen");
+                        brugerInput = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nTillykke! - Du har gættet det korrekte tal \"{randomNumber}\".");
+                        isValid = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"\nDet indtastede er ikke et heltal. Prøv igen.");
+                    brugerInput = Console.ReadLine();
+                }                
+            }
             Console.ReadKey();
         }
     }
